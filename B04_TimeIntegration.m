34e3d9a081da95e7a7d0df09e4ef149fee4cbb4f
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 21:56:12 +0100

    Update B04_TimeIntegration.m

--- B04_TimeIntegration.m
+++ B04_TimeIntegration.m
@@ -2,6 +2,19 @@ function [Sol] = B04_TimeIntegration(Calc,Veh)
 
 % Performs the time intregration of vehicle system traversing an irregularity profile
 
+% *************************************************************************
+% *** Script part of VEqMon2D tool for Matlab environment.              ***
+% *** Licensed under the GNU General Public License v3.0                ***
+% *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
+% *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. VEqMon2D - Equations of motion generation tool of 2D  ***
+% *** vehicles with Matlab, SoftwareX, Volume 19, 2022.                 ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101103                ***
+% ***                                                                   ***
+% *************************************************************************
+
 % -------------------------------------------------------------------------
 % ---- Input ----
 % Calc = Structure with Calculation variables, including at least:
