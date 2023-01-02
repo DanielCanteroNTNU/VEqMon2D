87bfa771ed0acbe6f291f778e4f4b3580560af40
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 21:56:02 +0100

    Update B03_ModalAnalysis.m

--- B03_ModalAnalysis.m
+++ B03_ModalAnalysis.m
@@ -2,6 +2,19 @@ function [Veh] = B03_ModalAnalysis(Veh)
 
 % Performing modal analysis of vehicle
 
+% *************************************************************************
+% *** Script part of VEqMon2D tool for Matlab environment.              ***
+% *** Licensed under the GNU General Public License v3.0                ***
+% *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
+% *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. VEqMon2D - Equations of motion generation tool of 2D  ***
+% *** vehicles with Matlab, SoftwareX, Volume 19, 2022.                 ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101103                ***
+% ***                                                                   ***
+% *************************************************************************
+
 % ---- Input ----
 % Veh = Structure variable with field
 %   .Sys = which contains the system matrices (M,C,K)
