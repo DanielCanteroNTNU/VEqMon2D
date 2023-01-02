9c9723c20c801edda35509a9429786554976de40
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 21:55:52 +0100

    Update B02_IndexedSymbVars.m

--- B02_IndexedSymbVars.m
+++ B02_IndexedSymbVars.m
@@ -3,6 +3,19 @@ function [] = B02_IndexedSymbVars(var_name,num_vars,varargin)
 % Script to create as many indexed symbolic variables as desired starting with a given name
 % Optional: all variables are then grouped into a vector with the name "var_name+i"
 
+% *************************************************************************
+% *** Script part of VEqMon2D tool for Matlab environment.              ***
+% *** Licensed under the GNU General Public License v3.0                ***
+% *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
+% *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. VEqMon2D - Equations of motion generation tool of 2D  ***
+% *** vehicles with Matlab, SoftwareX, Volume 19, 2022.                 ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101103                ***
+% ***                                                                   ***
+% *************************************************************************
+
 % ---- Inputs ----
 % var_name = String with the root name of the variable
 % num_vars = number of sybmolic variables to create
