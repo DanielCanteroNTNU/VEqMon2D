47371efa67c8b798c42307ef19964794feb98d0a
DanielCanteroNTNU <97284287+DanielCanteroNTNU@users.noreply.github.com>
2023-01-02 21:55:11 +0100

    Update A03_Simulation_Example.m

--- A03_Simulation_Example.m
+++ A03_Simulation_Example.m
@@ -1,5 +1,18 @@
 % Simulation of of a vehicle traveling at speed v over a irregularity profile
 
+% *************************************************************************
+% *** Script part of VEqMon2D tool for Matlab environment.              ***
+% *** Licensed under the GNU General Public License v3.0                ***
+% *** Author: Daniel Cantero (daniel.cantero@ntnu.no)                   ***
+% *** For help, modifications, and collaboration contact the author.    ***
+% ***                                                                   ***
+% *** If you found this tool useful, please cite:                       ***
+% *** D. Cantero. VEqMon2D - Equations of motion generation tool of 2D  ***
+% *** vehicles with Matlab, SoftwareX, Volume 19, 2022.                 ***
+% ***   DOI: https://doi.org/10.1016/j.softx.2022.101103                ***
+% ***                                                                   ***
+% *************************************************************************
+
 clear; clc;
 
 % -- Vehicle to run --
